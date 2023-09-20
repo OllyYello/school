05903c79afcf0031bb6865a63ecb03352e57d85f
OllyYello <Ollyyello@gmail.com>
2023-09-20 16:49:46 -0400

    Add files via upload

---
+++ Textbook.py
@@ -0,0 +1,11 @@
+""" This module is going to create & generate textbook data."""
+
+textbook_metadata = ('title', 'edition', 'author' 'publishing year', 'publisher', 'pages', 'price')
+
+textbook = []
+
+textbook.append((''' Python for Data Analysis''',
+                 '3rd edition',
+                 '''Wes Mckinney''',
+                 2022,
+                 '''
